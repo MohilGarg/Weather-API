8c660a9f1cf3ca110fabb8a30515e2afc81af13b
MohilGarg <80626694+MohilGarg@users.noreply.github.com>
2022-01-03 20:06:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function getDetails(place){
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById("name").innerHTML = abc.name)
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById("country").innerHTML = abc.sys.country)
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById("temperature").innerHTML = parseInt(abc.main.temp-273.15)+"°C")
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById("latitude").innerHTML = abc.coord.lat)
     
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById("longitude").innerHTML = abc.coord.lon)
 
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=736412f27570e2880986e590ae7278fc`)
     .then(abc => abc.json())
     .then(abc => document.getElementById("windspeed").innerHTML = abc.wind.speed+" km/h")
 
